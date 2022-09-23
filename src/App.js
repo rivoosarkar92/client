400bec412d141e7b577618aa18e46224b1bdbc78
rivoo.sarkar <rivoo.sarkar@accenture.com>
2022-09-23 16:34:46 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         });
       },[]);
   return (
-    <div className="App">
+    <div className="App slds-card">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="custom-para">{(!dataval) ? "Only changed in src" : dataval}</p>
