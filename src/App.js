891e07e1199f6503a7bacc68145af67dc11d068d
rivoo.sarkar <rivoo.sarkar@accenture.com>
2022-09-23 14:07:50 +0530

    reverting back

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import Cookies from 'universal-cookie';
 import logo from './drib_blink_bot.gif';
 import './App.css';
 import {Button } from '@salesforce/design-system-react';
-import {GlobalNavigationBar, GlobalNavigationBarRegion,GlobalNavigationBarLink}  from '@salesforce/design-system-react/';
+//import {GlobalNavigationBar, GlobalNavigationBarRegion,GlobalNavigationBarLink}  from '@salesforce/design-system-react/';
 function App() {
   const [dataval, setData] = React.useState(null);
-  const state = {
-		isOpen: false
-	};
-  console.log('@@@@@@==> : state '+JSON.stringify(state));
   React.useEffect(()=>{
     fetch('https://first-react-server-rs.herokuapp.com/my-custom-domain')
       .then((res)=>{
@@ -26,14 +22,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <GlobalNavigationBar>
-          <GlobalNavigationBarRegion region="primary">
-            <GlobalNavigationBarLink active label="Home" id="home-link" />
-            <GlobalNavigationBarLink label="Menu Item 2" />
-						<GlobalNavigationBarLink label="Menu Item 3" />
-						<GlobalNavigationBarLink label="Menu Item 4" />
-          </GlobalNavigationBarRegion>
-        </GlobalNavigationBar>
         <img src={logo} className="App-logo" alt="logo" />
         <p className="custom-para">{(!dataval) ? "Only changed in src" : dataval}</p>
         <Button label="Go!" variant="outline-brand"/>
