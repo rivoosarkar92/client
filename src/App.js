21024e1458c4915029438c4eeb13e55d103aa6ea
rivoo.sarkar <rivoo.sarkar@accenture.com>
2022-09-23 18:16:13 +0530

    convert

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Cookies from 'universal-cookie';
 import logo from './drib_blink_bot.gif';
 import './App.css';
 import {Button } from '@salesforce/design-system-react';
 
 //import {GlobalNavigationBar, GlobalNavigationBarRegion,GlobalNavigationBarLink}  from '@salesforce/design-system-react/';
-const App=()=>{
-  const [dataval, setData] = React.useState(null);
+class App extends React.Component{
+   dataval;
+  /*const [dataval, setData] = React.useState(null);
   React.useEffect(()=>{
     fetch('https://first-react-server-rs.herokuapp.com/my-custom-domain')
       .then((res)=>{
@@ -19,19 +20,33 @@ const App=()=>{
         }).catch((error)=>{
           console.log(error);
         });
-      },[]);
-      const dofunct=()=>{
-        alert('clicked me');
-      }
-  return (
+      },[]);*/
+connectedCallback() {
+    fetch('https://first-react-server-rs.herokuapp.com/my-custom-domain')
+      .then((res)=>{
+        return res.json();
+      })
+        .then((data)=>{
+          var cookies=new Cookies();
+          cookies.set('u-val', data['data']['o']['id'], { path: '/' });
+          this.dataval=cookies.get('u-val');
+        }).catch((error)=>{
+          console.log(error);
+        });
+  }
+  dofunct=()=>{
+    alert('clicked me');
+  }
+  render (){
+    return(
     <div className="App slds-card">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="custom-para">{(!dataval) ? "Only changed in src" : dataval}</p>
-        <Button onClick={dofunct} className="App-button" label="Go!" variant="outline-brand"/>
+        <p className="custom-para">{(!this.dataval) ? "Only changed in src" : this.dataval}</p>
+        <Button onClick={this.dofunct} className="App-button" label="Go!" variant="outline-brand"/>
       </header>
-    </div>
-  );
+    </div>);
+}
 }
 
 export default App;
