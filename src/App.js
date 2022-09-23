ef0a5288c525a58a38bd3c3bad8c14f798c9abd4
rivoo.sarkar <rivoo.sarkar@accenture.com>
2022-09-23 13:41:46 +0530

    new component use

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Cookies from 'universal-cookie';
 import logo from './drib_blink_bot.gif';
 import './App.css';
 import {Button } from '@salesforce/design-system-react';
-import {GlobalNavigationBar}  from '@salesforce/design-system-react/';
+import {GlobalNavigationBar, GlobalNavigationBarRegion,GlobalNavigationBarLink}  from '@salesforce/design-system-react/';
 
 function App() {
   const [dataval, setData] = React.useState(null);
@@ -23,11 +23,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <GlobalNavigationBar displayName="here">
+        <GlobalNavigationBar>
+          <GlobalNavigationBarRegion region="primary">
+            <GlobalNavigationBarLink active label="Home" id="home-link" />
+            <GlobalNavigationBarLink label="Menu Item 2" />
+						<GlobalNavigationBarLink label="Menu Item 3" />
+						<GlobalNavigationBarLink label="Menu Item 4" />
+          </GlobalNavigationBarRegion>
         </GlobalNavigationBar>
         <img src={logo} className="App-logo" alt="logo" />
         <p className="custom-para">{(!dataval) ? "Only changed in src" : dataval}</p>
-        <Button label="Go!" variant="brand"/>
+        <Button label="Go!" variant="outline-brand"/>
       </header>
     </div>
   );
