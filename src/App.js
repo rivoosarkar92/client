557b4f08128edaa62c3605c4896c6512c7c86b32
rivoo.sarkar <rivoo.sarkar@accenture.com>
2022-09-23 18:53:16 +0530

    convert

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-//import Cookies from 'universal-cookie';
+import Cookies from 'universal-cookie';
 import logo from './drib_blink_bot.gif';
 import './App.css';
 import {Button } from '@salesforce/design-system-react';
@@ -10,22 +10,25 @@ class App extends Component{
   dofunct=()=>{
     alert('clicked me');
   }
-  async callmethod(){
-    var ret=await fetch('https://first-react-server-rs.herokuapp.com/my-custom-domain')
-      return ret;
-  }
   render (){
-    var pro=this.callmethod();
-    console.log('RES : '+JSON.stringify(pro));
     return(
-    <div className="App slds-card">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {this.dataval}
-        <p className="custom-para">{(!this.dataval) ? "Only changed in src" : this.dataval}</p>
-        <Button onClick={this.dofunct} className="App-button" label="Go!" variant="outline-brand"/>
-      </header>
-    </div>);
+      fetch('https://first-react-server-rs.herokuapp.com/my-custom-domain').then((res)=>{
+        return res.json()
+      }).then((data)=>{
+          var cookies=new Cookies();
+          cookies.set('u-val', data['data']['o']['id'], { path: '/' });
+          this.dataval=cookies.get('u-val');
+          <div className="App slds-card">
+          <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p className="custom-para">{(!this.dataval) ? "Only changed in src" : this.dataval}</p>
+          <Button onClick={this.dofunct} className="App-button" label="Go!" variant="outline-brand"/>
+        </header>
+    </div>
+      }).catch((err)=>{
+        console.log('ERROR ==> : '+err);
+      })
+    );
 }
 }
 
