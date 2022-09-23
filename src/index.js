d88e80e38c002ab3b844b74c25b5a334c5af2c58
rivoo.sarkar <rivoo.sarkar@accenture.com>
2022-09-23 14:32:07 +0530

    another

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Button } from '@salesforce/design-system-react';
+const hello=(props)=>{
+  return <h1>Hi</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <hello/>
     <center>
     <Button label="Go!" variant="outline-brand"/>
     </center>
