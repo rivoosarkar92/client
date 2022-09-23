01fc7dedcf0610ee3fb9aaf9259d7a7b7139096a
rivoo.sarkar <rivoo.sarkar@accenture.com>
2022-09-23 14:19:51 +0530

    new

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <center>
     <Button label="Go!" variant="outline-brand"/>
+    </center>
   </React.StrictMode>
 );
 
