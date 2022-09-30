2377da483ed4608b471198cd068db841d7f92eed
rivoo.sarkar <rivoo.sarkar@accenture.com>
2022-09-30 10:06:28 +0530

    net

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import netlifyIdentity from 'netlify-identity-widget';
 
 function Eello(props){
   //console.log('@@@==> PROPS : '+JSON.stringify(props));
